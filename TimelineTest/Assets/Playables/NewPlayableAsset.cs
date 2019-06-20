786f6733ce67b9c930e032f316396d79084f3e91
masha <mashacoder@foxmail.com>
2019-06-20 10:11:20 +0800

    update timeline study

---
+++ TimelineTest/Assets/Playables/NewPlayableAsset.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class NewPlayableAsset : PlayableAsset
+{
+    public ExposedReference<Text> title_text;
+    // [Multiline(3)]
+    public string title_str;
+
+    public ExposedReference<Text> name_text;
+    public string name_str;
+
+    // Factory method that generates a playable based on this asset
+    public override Playable CreatePlayable(PlayableGraph graph, GameObject go)
+    {
+        NewPlayableBehaviour pb = new NewPlayableBehaviour();
+        pb.title_text = title_text.Resolve(graph.GetResolver());
+        pb.title_str = title_str;
+
+        pb.name_text = name_text.Resolve(graph.GetResolver());
+        pb.name_str = name_str;
+
+        // return Playable.Create(graph);
+        return ScriptPlayable<NewPlayableBehaviour>.Create(graph, pb);
+    }
+}
---
+++ TimelineTest/Assets/Playables/NewPlayableBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.UI;
+
+// A behaviour that is attached to a playable
+public class NewPlayableBehaviour : PlayableBehaviour
+{
+    public Text title_text;
+    public string title_str;
+    public Text name_text;
+    public string name_str;
+
+    // Called when the owning graph starts playing
+    public override void OnGraphStart(Playable playable)
+    {
+        
+    }
+
+    // Called when the owning graph stops playing
+    public override void OnGraphStop(Playable playable)
+    {
+        
+    }
+
+    // Called when the state of the playable is set to Play
+    public override void OnBehaviourPlay(Playable playable, FrameData info)
+    {
+        if (title_text != null && name_text != null)
+        {
+            title_text.text = title_str;
+            title_text.gameObject.SetActive(true);
+            name_text.text = name_str;
+            name_text.gameObject.SetActive(true);
+        }
+    }
+
+    // Called when the state of the playable is set to Paused
+    public override void OnBehaviourPause(Playable playable, FrameData info)
+    {
+        if (title_text != null && name_text != null)
+        {
+            title_text.gameObject.SetActive(false);
+            name_text.gameObject.SetActive(false);
+        }
+    }
+
+    // Called each frame while the state is set to Play
+    public override void PrepareFrame(Playable playable, FrameData info)
+    {
+        
+    }
+}
